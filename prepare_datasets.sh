3d285f2d0ca1283402ee33d8afecab1a6f9cbfab
AzimuthLi <shiyi.li@ifu.baug.ethz.ch>
2024-06-18 20:54:12 +0200

    upload files

---
+++ prepare_datasets.sh
@@ -0,0 +1,6 @@
+# models
+kaggle datasets download -d verracodeguacas/huggingfacedebertav3variants -p ./input --unzip
+# persuade 2.0 corpus
+kaggle datasets download -d deepaksingh47/persuade-2-0 -p ./input --unzip
+# competition dataset
+kaggle competitions download -c learning-agency-lab-automated-essay-scoring-2 -p ./input --unzip
